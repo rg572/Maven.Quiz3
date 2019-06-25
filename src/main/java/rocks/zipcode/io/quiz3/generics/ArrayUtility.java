ebbf575102b639722f7cc9e48ee78d67b7974c3a
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-25 14:08:14 -0400

    ArrayUtility passing all tests

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,6 +1,11 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @author leon on 09/12/2018.
@@ -13,18 +18,54 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
+        boolean[] beenChecked = new boolean[array.length];
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]) {
+                Integer count = 1;
+                for(int j = i + 1; j < array.length; j++){
+                    if(array[i].equals(array[j])){
+                        count++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(count%2==1){
+                    return array[i];
+                }
+            }
+
+        }
         return null;
     }
 
     public SomeType findEvenOccurringValue() {
+        boolean[] beenChecked = new boolean[array.length];
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]) {
+                Integer count = 1;
+                for(int j = i + 1; j < array.length; j++){
+                    if(array[i].equals(array[j])){
+                        count++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(count%2==0){
+                    return array[i];
+                }
+            }
+
+        }
         return null;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        return (int)Arrays.stream(array).filter((val) -> val.equals(valueToEvaluate)).count();
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        return null;
+
+        return  Arrays.stream(array).filter((val) -> predicate.apply(val)).
+                toArray((i)->(SomeType[]) Array.newInstance(array[0].getClass(), i));
+
     }
+
 }
